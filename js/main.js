6e598a64306571065406ab64dfa39ce7091ed72a
BourBonBoi <artemmine9292@gmail.com>
2024-02-10 18:16:46 +0900

    add slider and to-do list

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+
 const number = document.getElementById('number');
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container'); 
+
+/*number*/ 
 let changeNum = Number(number.textContent)
 
 function minusNumber() {
@@ -13,12 +18,56 @@ function addNumber() {
     saveData() 
 }
 
+/*todo*/ 
+function addTask() {
+    if (inputBox.value === '') {
+        alert('Please add new task');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = 'X';
+        li.appendChild(span);
+    }
+    
+    inputBox.value = '';
+    saveData()
+}
+
+listContainer.addEventListener('click', function(e){
+    if (e.target.tagName ==='LI') {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName ==='SPAN'){
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false);
 
 
 function saveData() {
-    localStorage.setItem('number', number.innerHTML);
+    localStorage.setItem('data', listContainer.innerHTML);
 }
 function showList() {
-    number.innerHTML = localStorage.getItem('number');
+    listContainer.innerHTML = localStorage.getItem('data');
 }
+
+/*Swiper*/
+
+let swiper = new Swiper('.mySwiper', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+
+
+
+
+
 showList();
+
